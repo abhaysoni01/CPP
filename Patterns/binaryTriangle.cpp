b958768361528fc59908f96d48a5aab3bfa7ac51
abhay soni01 <abhaysoni@gmail.com>
2025-12-10 13:31:14 +0530

    new codes

---
+++ Patterns/binaryTriangle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 1;i<=n;i++){
+        if(i%2 == 1){
+            for(int j =1;j<=i;j++){
+                if(j%2 == 1) cout<<1;
+                else cout<<0;
+            }
+        }
+        else{
+            for(int j =1;j<=i;j++){
+                if(j%2 == 1) cout<<0;
+                else cout<<1;
+            }
+
+        }
+        cout<<endl;
+    }
+}
---
+++ Patterns/dimond.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+//     * 
+//    * * 
+//   * * * 
+//  * * * * 
+// * * * * *
+//  * * * *
+//   * * *
+//    * *
+//     *
+int main(){
+    int n;
+    cin>>n;
+    for(int i =1;i<=n;i++){
+        //space
+        for(int j = 1;j<=n-i;j++){
+            cout<<" ";
+        }
+        //star
+        for(int k = 1;k<=i;k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i =1;i<=n;i++){
+        //space
+        for(int j = 1;j<=i;j++){
+            cout<<" ";
+        }
+        //star
+        for(int k = 1;k<=n-i;k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
