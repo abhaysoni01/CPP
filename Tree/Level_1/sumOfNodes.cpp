8a6870a18ff055be3a2d27568dcc5096ca52aab5
abhaysoni01 <abhaysoni11504@gmail.com>
2026-05-02 13:38:12 +0530

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

---
+++ Tree/Level_1/sumOfNodes.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+static int i = -1;
+Node* buildtree(vector<int>& arr){
+    i++;
+    if(arr[i] == -1){
+        return NULL;
+    }
+    Node* curr = new Node(arr[i]);
+    curr->left = buildtree(arr);
+    curr->right = buildtree(arr);
+    return curr;
+}
+
+void printLevelOrder(Node* root){
+    if(!root) return ;
+    queue<Node*> Q;
+    Q.push(root);
+    Q.push(NULL);
+    while(!Q.empty()){
+        Node* curr = Q.front();
+        Q.pop();
+        if(curr == NULL){
+            cout << endl;
+            if(Q.empty()) break;
+            Q.push(NULL);
+        }
+        else{
+            cout << curr->data <<" ";
+            if(curr->left != NULL){
+                Q.push(curr->left);
+            }
+            if(curr->right != NULL){
+                Q.push(curr->right);
+            }
+        }
+    }
+    cout << endl;
+}
+int sumOfNodes(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftSum = sumOfNodes(root->left);
+    int rightSum = sumOfNodes(root->right);
+    return leftSum + rightSum + root->data;
+}
+int main(){
+    vector<int> arr = {1,2,4,-1,-1,5,-1,-1,3,-1,6,7,-1,-1,8,-1,-1};
+    Node* root = buildtree(arr);
+    printLevelOrder(root);
+    // cout << "Height of Tree: "<<height(root)<<endl;
+    cout << "Sum of Nodes: "<< sumOfNodes(root) << endl;
+    return 0;
+}
